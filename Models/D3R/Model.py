f7ec3fe79425377e6c2ec8341a6092cd34de9339
Hit_Hyacinth <942891895@qq.com>
2024-05-26 17:07:34 +0800

    2024

---
+++ Models/D3R/Model.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+
+class D3R(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+
+    def forward(self,input):
+        pass
+
+
+    def fit(self):
+        pass
+
+
+    def test(self):
+        pass
+
