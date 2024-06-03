5061c388696f60830910ff15218245680c1181e4
Hit_Hyacinth <942891895@qq.com>
2024-06-03 16:21:19 +0800

    2024

--- Models/BaseModel.py
+++ Models/BaseModel.py
@@ -11,15 +11,23 @@ class BaseModel(nn.Module):
     def __init__(self):
         super(BaseModel,self).__init__()
 
-    def shuffle(self,data):
+    def shuffle(self,data,dim = 1):
+        '''
+        :param data: the data need to shuffle
+        :param dim: dim to shuffle. For time series anomaly detection,the value could only be 0 / 1, namely shuffle along by batch or sequence.
+        :return:
+        '''
+
         # 假设你的张量数据
         (batch_size,seq_len,channels) = data.shape
         # 生成随机排列的索引
-        indices = torch.randperm(batch_size)
+        indices = torch.randperm(data.shape[dim])
 
         # 对时间步维度进行打乱
-        data = data[indices]
-
+        if dim == 0:
+            data = data[indices]
+        elif dim == 1:
+            data = data[:,indices,:]
         return data
 
 
@@ -58,3 +66,20 @@ class BaseModel(nn.Module):
             wirteLog(self.config["base_path"] + "/Logs/" + identifier, "evaluate", {"result": result})
 
         return f1
+
+    def ema(self,data,alpha = 0.9):
+        '''
+        指数滑动平均
+        :param data: shape is [batch_size,seq_len,channels].
+        :param alpha: decay value.
+        :return:  data after Exponential Moving Average, EMA . data shape is[batch_size,seq_len,channels]
+        '''
+
+        (batch_size,seq_len,channels) = data.shape
+        ema = data.clone().detach()
+        for t in range(1, seq_len):
+            ema[:, t, :] = alpha * data[:, t, :] + (1 - alpha) * ema[:, t - 1, :]
+            correction_factor = 1 - (1 - alpha) ** (t)
+            ema[:, t, :] = ema[:, t, :] / correction_factor
+
+        return ema
