500fc76b56feac069aaf3c8dcc5c145645fda6c5
Hit_Hyacinth <942891895@qq.com>
2024-06-17 18:08:13 +0800

    2024

--- Evaluate.py
+++ Evaluate.py
@@ -482,7 +482,7 @@ def getConfigs():
 
             "num_filters":3,
             "kernel_size":3,
-        
+
             "kernel": "rbf",
             "gamma": "auto",
             "degree": 3,
@@ -516,7 +516,7 @@ def count_parameters(model):
 
 def evalOneDataset(dataset_name):
     config = getConfigs()
-    model_list = ["TRANSFORMER","TCNAE",'PCA',"IForestAD","UAE","TRANAD","OmniAnomaly","OCSVM","MSCRED","LSTMVAE","LSTMAE","NASALSTM","DAGMM",]
+    model_list = ["TCNAE",'PCA',"IForestAD","UAE","TRANAD","OmniAnomaly","OCSVM","MSCRED","LSTMVAE","LSTMAE","NASALSTM","DAGMM","TRANSFORMER"]
     base_path = os.path.dirname(os.path.abspath(__file__))
     #get data
 
