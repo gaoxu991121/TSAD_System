813c81038b6d06bdb40909207db18ce09066f2a7
Hit_Hyacinth <942891895@qq.com>
2024-07-06 10:59:35 +0800

    2024

---
+++ Models/Layers/LoRALayer.py
@@ -0,0 +1,26 @@
+import torch
+
+
+class LoRALayer(torch.nn.Module):
+    def __init__(self, in_dim, out_dim, rank, alpha):
+        super().__init__()
+        std_dev = 1 / torch.sqrt(torch.tensor(rank).float())
+        self.A = torch.nn.Parameter(torch.randn(in_dim, rank) * std_dev)
+        self.B = torch.nn.Parameter(torch.zeros(rank, out_dim))
+        self.alpha = alpha
+
+    def forward(self, x):
+        x = self.alpha * (x @ self.A @ self.B)
+        return x
+
+class LinearWithLoRA(torch.nn.Module):
+    def __init__(self, linear, rank, alpha,input_size,output_size):
+        super().__init__()
+        self.linear = linear
+        self.lora = LoRALayer(
+            input_size, output_size, rank, alpha
+        )
+
+    def forward(self, x):
+        x = self.lora(x)
+        return self.linear(x)
